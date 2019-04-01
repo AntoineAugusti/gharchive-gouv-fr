4380f7df8c1af2f5706ab55e5aaf6e1f22566223
Antoine Augusti <antoine.augusti@data.gouv.fr>
2019-04-01 17:37:07 +0200

    Improve Bash script

--- run.sh
+++ run.sh
@@ -6,6 +6,8 @@ MONTH=$1
 cd data
 rm -f $MONTH.json
 
+wget -nc https://data.gharchive.org/$MONTH-{29..31}-{0..23}.json.gz || true
+
 for i in $(seq -f "%02g" 1 28)
 do
   wget -nc https://data.gharchive.org/$MONTH-$i-{0..23}.json.gz
@@ -14,7 +16,6 @@ do
   rm -f $MONTH-$i-*.json
 done
 
-wget -nc https://data.gharchive.org/$MONTH-{29..31}-{0..23}.json.gz || true
 gunzip -f $MONTH-*.json.gz
 cat $MONTH-*.json | jq -c 'select( .type == "PushEvent" ) | select(.payload.commits[0].author.email // "" | endswith(".gouv.fr"))' >> $MONTH.json
 rm -f $MONTH-*
