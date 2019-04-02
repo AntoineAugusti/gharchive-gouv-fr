c021ff319ef0aeec5a205d926a5d41f62664e711
Antoine Augusti <antoine.augusti@data.gouv.fr>
2019-04-02 16:18:49 +0200

    Aggregate Python script

---
+++ aggregate.py
@@ -0,0 +1,32 @@
+import csv
+import json
+
+data = []
+
+with open("all.json") as f:
+    for line in f:
+        parsed = json.loads(line)
+
+        email = parsed["payload"]["commits"][0]["author"]["email"]
+
+        current = {}
+        current["id"] = parsed["id"]
+        current["repository_name"] = parsed["repo"]["name"]
+        current["ref"] = parsed["payload"]["ref"]
+        current["author_email"] = email
+        current["author_domain"] = email.split("@")[1]
+        current["author_name"] = parsed["payload"]["commits"][0]["author"]["name"]
+        current["message"] = parsed["payload"]["commits"][0]["message"]
+        current["sha"] = parsed["payload"]["commits"][0]["sha"]
+        if "org" in parsed:
+            current["organisation_name"] = parsed["org"]["login"]
+        else:
+            current["organisation_name"] = None
+        current["created_at"] = parsed["created_at"]
+
+        data.append(current)
+
+with open("commits.csv", "w") as f:
+    writer = csv.DictWriter(f, fieldnames=data[0].keys())
+    writer.writeheader()
+    writer.writerows(data)
