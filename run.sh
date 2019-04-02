5ed96cc96744ea6746ac1a6bc69126a0a553d8fb
Antoine Augusti <antoine.augusti@data.gouv.fr>
2019-04-02 10:31:19 +0200

    Ignore error if not found

--- run.sh
+++ run.sh
@@ -10,7 +10,7 @@ wget -nc https://data.gharchive.org/$MONTH-{29..31}-{0..23}.json.gz || true
 
 for i in $(seq -f "%02g" 1 28)
 do
-  wget -nc https://data.gharchive.org/$MONTH-$i-{0..23}.json.gz
+  wget -nc https://data.gharchive.org/$MONTH-$i-{0..23}.json.gz || true
   gunzip -f $MONTH-$i-*.json.gz
   cat $MONTH-$i-*.json | jq -c 'select( .type == "PushEvent" ) | select(.payload.commits[0].author.email // "" | endswith(".gouv.fr"))' >> $MONTH.json
   rm -f $MONTH-$i-*.json
